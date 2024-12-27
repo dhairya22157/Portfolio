6327c21413b410ae014061aa9e3c232403933807
dhairya22157 <dhairya22157@iiitd.ac.in>
2024-12-27 23:38:39 +0530

    Fix import case for Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import About from "./components/About/About";
 import Experience from "./components/Experience/Experience";
 import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
-import Navbar from "./components/navbar/navbar";
+import Navbar from "./components/Navbar/Navbar";
 import Projects from "./components/Projects/Projects";
 import ContactForm from "./components/ContactForm/ContactForm";
 
